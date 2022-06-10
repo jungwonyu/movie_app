355054c3229c9cd368d7b338498285e28af0a863
jungwonyu <sunsetinmygarden@gmail.com>
2022-06-10 21:36:37 +0900

    컴포넌트와 props이해, JSX 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <img scr={picture} />
+    </div>
+  );
+}
+
+const foodLike = [
+  {
+    name: "Kimchi",
+    image:
+      "https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20211214_272%2F1639455797629bOiHk_JPEG%2F40591693355469890_494284185.jpg&type=a340.jpg",
+  },
+  {
+    name: "Ramen",
+    image:
+      "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodLike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
