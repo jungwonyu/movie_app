8d922067a40535766f61e830330bfc775f382993
jungwonyu <sunsetinmygarden@gmail.com>
2022-06-16 22:49:03 +0900

    Updates

---
+++ precache-manifest.10f37fa3130d8ccde370908bb2546879.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc7424939d2aecb0914f",
+    "url": "/movie_app/static/css/main.6761ec7b.chunk.css"
+  },
+  {
+    "revision": "fc7424939d2aecb0914f",
+    "url": "/movie_app/static/js/main.fc742493.chunk.js"
+  },
+  {
+    "revision": "ba066dc5f93291cd000e",
+    "url": "/movie_app/static/js/1.ba066dc5.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "ed5d7621ba93317984060bdd655a8e6b",
+    "url": "/movie_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.86ef319557cd7d03865f7469e2f8ff92.js"
+  "/movie_app/precache-manifest.10f37fa3130d8ccde370908bb2546879.js"
 );
 
 workbox.clientsClaim();
